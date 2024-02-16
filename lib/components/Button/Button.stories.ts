74d3b88f6f3d10a16a49afce316e98eb25aef548
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-15 21:39:24 -0300

    fix: Test pacth bump

--- lib/components/Button/Button.stories.ts
+++ lib/components/Button/Button.stories.ts
@@ -19,7 +19,6 @@ const meta = {
 } satisfies Meta<typeof Button>;
 
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
