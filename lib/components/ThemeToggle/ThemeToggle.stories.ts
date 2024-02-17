320f3f04eefd39207737186206b50b67fb8b4557
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-17 15:23:52 -0300

    Change stories type

--- lib/components/ThemeToggle/ThemeToggle.stories.ts
+++ lib/components/ThemeToggle/ThemeToggle.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeToggle } from ".";
 
-const meta: Meta<typeof ThemeToggle> = {
+const meta = {
   component: ThemeToggle,
   parameters: { layout: "centered" },
   tags: ["autodocs"],
-};
+} satisfies Meta<typeof ThemeToggle>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
