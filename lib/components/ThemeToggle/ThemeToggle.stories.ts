65be4156180f8ca108a8a60d933c8a243fce2915
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-17 15:36:59 -0300

    Test actions

--- lib/components/ThemeToggle/ThemeToggle.stories.ts
+++ lib/components/ThemeToggle/ThemeToggle.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeToggle } from ".";
 
-const meta = {
+const meta: Meta<typeof ThemeToggle> = {
   component: ThemeToggle,
   parameters: { layout: "centered" },
   tags: ["autodocs"],
-} satisfies Meta<typeof ThemeToggle>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
