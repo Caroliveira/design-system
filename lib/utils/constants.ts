92698d9024f35522cc2ca4b97fcd6ad6a09398d9
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-18 10:03:02 -0300

    Improve color accessibility and add reset css

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -2,9 +2,9 @@ export const disableStorybookArg = { table: { disable: true } };
 
 export const defaultColors = {
   lightShades: "#EBE9E5",
-  lightAccent: "#858486",
+  lightAccent: "#B6B4B6",
   brandColor: "#AD4022",
-  brandAccent: "#C17541",
+  brandAccent: "#C37846",
   darkAccent: "#343434",
   darkShades: "#242424",
   success: "#6A8E43",
