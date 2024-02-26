a5907f94c2bd54198340046eaee1c2fdd350e121
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-26 12:25:29 -0300

    Fix sb background change to impact components

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,19 +1,31 @@
 import type { Preview } from "@storybook/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useTheme } from "../lib/context/ThemeContext";
 import { ThemeProvider } from "../lib/context/ThemeProvider";
 import TemplateDocs from "../lib/stories/template.mdx";
 
+const withThemeProvider = (Story) => (
+  <ThemeProvider>
+    <Story />
+  </ThemeProvider>
+);
+
+const withThemeContext = (Story, context) => {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const { value } = context.globals.backgrounds || {};
+    if (value) setTheme(value === "#F8F8F8" ? "light" : "dark");
+  }, [context.globals.backgrounds]);
+
+  return <Story />;
+};
+
 const preview: Preview = {
-  decorators: [
-    (Story) => (
-      <ThemeProvider fixedTheme="dark">
-        <Story />
-      </ThemeProvider>
-    ),
-  ],
+  decorators: [withThemeContext, withThemeProvider],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    backgrounds: { default: "dark", disable: true },
+    backgrounds: { default: "dark" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
