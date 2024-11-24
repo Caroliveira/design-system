18f14ae69cddec8ed9bc4b511831bc2168b48665
Carolina Oliveira <linaribeirooliveira@gmail.com>
2024-11-24 19:28:36 -0300

    Add navbar component

---
+++ lib/components/Navbar/index.tsx
@@ -0,0 +1,82 @@
+import styles from "./Navbar.module.css";
+
+export const Navbar = ({
+  children,
+  className = "",
+  ...rest
+}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) => {
+  const navbarClass = [styles.nav, className].join(" ");
+  return (
+    <nav className={navbarClass} {...rest}>
+      {children}
+    </nav>
+  );
+};
+
+export type NavbarMenuProps = {
+  openNav: boolean;
+  setOpenNav: React.Dispatch<React.SetStateAction<boolean>>;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+Navbar.Menu = ({
+  className = "",
+  openNav,
+  setOpenNav,
+  "aria-controls": ariaControls = "navigation",
+  ...rest
+}: NavbarMenuProps) => {
+  const navbarMenuClass = [styles.nav__menu, className].join(" ");
+  return (
+    <button
+      className={navbarMenuClass}
+      aria-expanded={openNav}
+      aria-controls={ariaControls}
+      {...rest}
+      onClick={() => setOpenNav(!openNav)}
+    >
+      <div className={styles[`nav__menu${openNav ? "--close" : "--open"}`]} />
+    </button>
+  );
+};
+
+export type NavbarListProps = {
+  openMobileNav?: boolean;
+} & React.HTMLAttributes<HTMLUListElement>;
+
+Navbar.List = ({
+  children,
+  className = "",
+  openMobileNav,
+  id = "navigation",
+  ...rest
+}: NavbarListProps) => {
+  const navbarListClass = [styles.nav__list, className].join(" ");
+  if (!openMobileNav) return <></>;
+  return (
+    <ul id={id} className={navbarListClass} {...rest}>
+      {children}
+    </ul>
+  );
+};
+
+export type NavbarListItemProps = {
+  isActive: boolean;
+} & React.LiHTMLAttributes<HTMLLIElement>;
+
+Navbar.ListItem = ({
+  children,
+  className = "",
+  isActive,
+  ...rest
+}: NavbarListItemProps) => {
+  const navbarListItemClass = [
+    styles[`nav__list__item${isActive ? "--current" : ""}`],
+    className,
+  ].join(" ");
+
+  return (
+    <li className={navbarListItemClass} {...rest}>
+      {children}
+    </li>
+  );
+};
--- lib/main.ts
+++ lib/main.ts
@@ -4,6 +4,13 @@ export { Button } from "./components/Button";
 export type { SwitchProps } from "./components/Switch";
 export { Switch } from "./components/Switch";
 
+export type {
+  NavbarListItemProps,
+  NavbarListProps,
+  NavbarMenuProps,
+} from "./components/Navbar";
+export { Navbar } from "./components/Navbar";
+
 export type { ThemeProviderProps } from "./context/ThemeProvider";
 export { ThemeProvider } from "./context/ThemeProvider";
 export { useTheme } from "./context/ThemeContext";
